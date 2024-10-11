512b27aa8f7fd7a166f12b7ff1301dad68409609
Syed Ibad Ali <123474911+SyedIbad123@users.noreply.github.com>
2024-10-11 03:23:46 -0700

    Update UserController.js with CRUD

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,50 @@
 // const { Op } = require("sequelize");
+const db = require("../models/index.js");
 const UserRepo = require("../repos/UserRepo.js");
-const { validateCreateUser } = require("../validators/UserValidator.js");
+const {
+  validateCreateUser,
+  validateUpdateUser,
+} = require("../validators/UserValidator.js");
 const BaseController = require("./BaseController.js");
 
 class UserController extends BaseController {
   constructor() {
     super();
   }
+
+  getUserById = async (req, res) => {
+    const { id } = req?.params;
+    const user = await UserRepo.findById(id);
+
+    if (!user) {
+      return this.errorResponse(res, "User ID not found", 404);
+    }
+
+    return this.successResponse(res, user, "User retrieved successfully");
+  };
+
+  getAllUsers = async (req, res) => {
+    const {
+      sortBy = "id",
+      sortOrder = "DESC",
+      page = 1,
+      limit = 10,
+      search = "",
+      filterByName,
+      filterByEmail,
+      filterByStatus,
+    } = req?.query;
+
+    const skip = (page - 1) * limit;
+    const searchParams = {};
+
+    if (search) {
+      searchParams[db.sequelize.Op.or] = [{}];
+    }
+
+    const users = await UserRepo.getUsers();
+    return this.successResponse(res, users, "Users retrieved successfully");
+  };
 
   createUser = async (req, res) => {
     const validationResult = validateCreateUser(req.body);
@@ -17,12 +55,55 @@ class UserController extends BaseController {
 
     const { profile, ...userData } = req.body;
 
-    const user = await UserRepo.createUserAndProfile({
-      ...userData,
-      profile: { ...profile },
-    });
+    try {
+      const user = await UserRepo.createUserAndProfile({
+        ...userData,
+        profile: {
+          ...profile,
+        },
+      });
+      return this.successResponse(res, user, "User created successfully");
+    } catch (e) {
+      throw new Error("Error creating user and user profile");
+    }
+  };
 
-    return this.successResponse(res, user, "User created successfully");
+  updateUser = async (req, res) => {
+    const { id } = req.params;
+    const validationResult = validateUpdateUser(req.body);
+
+    if (!validationResult.status) {
+      return this.validationErrorResponse(res, validationResult.message);
+    }
+
+    const isUser = await UserRepo.isUserExists(id);
+
+    if (!isUser) {
+      return this.errorResponse(res, "User ID not found", 404);
+    }
+
+    const user = await UserRepo.updateUser(req.body, id);
+    return this.successResponse(res, user, "User updated successfully");
+  };
+
+  deleteUser = async (req, res) => {
+    let { id } = req?.params;
+    let { type } = req?.query;
+
+    const isUser = await UserRepo.isUserExists(id);
+
+    if (!isUser) {
+      return this.errorResponse(res, "User ID not found", 404);
+    }
+
+    type = type ? type : "soft";
+
+    const user = await UserRepo.deleteUser(id, type);
+    return this.successResponse(
+      res,
+      user,
+      `User with ID ${id} deleted successfully`
+    );
   };
 }
 
