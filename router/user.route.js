0508f9952858f67ebed147c6709a354932d3a458
Syed Ibad Ali <123474911+SyedIbad123@users.noreply.github.com>
2024-10-11 03:25:36 -0700

    Update user.route.js with CRUD

--- router/user.route.js
+++ router/user.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController.js");
 
-// router.get("/get-all-users", UserController.getAllUser);
-// router.get("/get-user/:id", UserController.getUserById);
+router.get("/get-user/:id", UserController.getUserById);
+router.get("/get-all-users", UserController.getAllUsers);
 router.post("/create-user", UserController.createUser);
-// router.patch("/update-user/:id", UserController.updateUser);
-// router.delete("/delete-user/:id", UserController.deleteUser);
+router.patch("/update-user/:id", UserController.updateUser);
+router.delete("/delete-user/:id", UserController.deleteUser);
 
 module.exports = router;
